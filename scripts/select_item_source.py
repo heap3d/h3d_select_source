8364839461c23ace56c93ba412f560dfd1af159b
Dmytro Holub <heap3d@gmail.com>
2024-11-30 15:37:14 +0200

    updated: get_instance_source() get_replicator_prototypes() get_replicator_point_source()

--- scripts/select_item_source.py
+++ scripts/select_item_source.py
@@ -7,25 +7,41 @@
 # select item source
 # ================================
 
-from typing import Iterable
+from typing import Iterable, Union
+
 import modo
+
+from h3d_utilites.scripts.h3d_debug import h3dd
 
 
 def main():
-    ...
+    selected = modo.Scene.selected
+
+    source_items = []
+    for item in selected:
+        source = get_instance_source(item)
+        if source:
+            source_items.append(source)
 
 
-def get_instance_source(instance: modo.Item) -> modo.Item:
-    ...
+def get_instance_source(instance: modo.Item) -> Union[None, modo.Item]:
+    try:
+        return instance.itemGraph('source').forward()[0]
+    except IndexError:
+        return None
 
 
 def get_replicator_prototypes(replicator: modo.Item) -> Iterable[modo.Item]:
-    ...
+    return replicator.itemGraph('particle').forward()
 
 
-def get_replicator_point_source(replicator: modo.Item) -> modo.Item:
-    ...
+def get_replicator_point_source(replicator: modo.Item) -> Union[None, modo.Item]:
+    try:
+        return replicator.itemGraph('particle').reverse()[0]
+    except IndexError:
+        return None
 
 
 if __name__ == '__main__':
+    h3dd.enable_debug_output(False)
     main()
